2bca20eac05b5613a456a1b72d6ee6bca41f0faa
Zdenek Hynek <zdenek.hynek@gmail.com>
2024-02-20 18:11:58 +0000

    Add d3 as dependency

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import * as d3 from "d3";
 
-setupCounter(document.querySelector('#counter'))
+const dataset = [5, 10, 15, 20, 25];
+
+d3.select("body")
+  .selectAll("p")
+  .data(dataset)
+  .enter()
+  .append("p")
+  .text((d) => `New paragraph ${d}`);
