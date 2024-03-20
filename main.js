3a03bf0e06228460adb54cac82a92acd663071be
Zdenek Hynek <zdenek.hynek@gmail.com>
2024-03-20 08:55:08 +0000

    Chart axis

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ import "./style.css";
 // import join from "./src/02_join";
 // join();
 
-import scales from "./src/03_scales";
-scales();
+// import scales from "./src/03_scales";
+// scales();
+
+import axis from "./src/04_axis";
+axis();
--- src/04_axis.js
+++ src/04_axis.js
@@ -1,10 +1,20 @@
 import * as d3 from "d3";
 
-export default function axis() {
+export default function scales() {
+  var margins = { top: 20, bottom: 20, left: 50, right: 10 };
   var svg = d3.select("svg");
+  var g = svg
+    .select("g")
+    .style("transform", `translate(${margins.left}px, ${margins.top}px)`);
+
+  var svgWidth = svg.attr("width");
+  var svgHeight = svg.attr("height");
+  var chartWidth = svgWidth - margins.left - margins.right;
+  var chartHeight = svgHeight - margins.top - margins.bottom;
+  console.log("svgHeight", svgHeight);
 
   d3.csv("../data/data-wb.csv").then(function (data) {
-    var circle = svg.selectAll("circle").data(data);
+    var circle = g.selectAll("circle").data(data);
     var circleEnter = circle.enter().append("circle");
     var circleUpdate = circle.merge(circleEnter);
     circleUpdate.classed("red", true);
@@ -25,8 +35,8 @@ export default function axis() {
     //  create scales where the first parameter is an array with a minimum and maximum values
     //  from the dataset (input domain - e.g. what goes in) and the second parameter
     //  is an array with size of the svg (output range - e.g. what comes out)
-    var xScale = d3.scaleLinear(xMinMax, [0, 760]);
-    var yScale = d3.scaleLinear(yMinMax, [640, 0]);
+    var xScale = d3.scaleLinear(xMinMax, [0, chartWidth]);
+    var yScale = d3.scaleLinear(yMinMax, [chartHeight, 0]);
 
     //  @TODO: create a d3.scaleSqrt to set the radius of the circle on line 42
 
@@ -42,8 +52,19 @@ export default function axis() {
       return Math.sqrt(d[rKey] / 100000);
     });
 
-    //  @TODO: set circle fill color based on some data attribute, e.g. "Region"
+    svg
+      .append("g")
+      .classed("x-axis", true)
+      .attr("transform", `translate(${margins.left}, ${chartHeight})`)
+      .call(d3.axisBottom(xScale).tickSize(-1 * chartHeight));
 
+    svg
+      .append("g")
+      .classed("y-axis", true)
+      .attr("transform", `translate(${margins.left},0)`)
+      .call(d3.axisLeft(yScale).tickSize(-1 * chartWidth));
+
+    //  @TODO: set circle fill color based on some data attribute, e.g. "Region"
 
     //  always add exit
     circleUpdate.exit().remove();
